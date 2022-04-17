8dcca4cc0bcdeb88bf1ca82c85596edaa3352edb
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-17 12:12:04 +0000

    add task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,2 +1,9 @@
 # config ssh with pp
 
+exec {'sudo sed -i "s/^.*PasswordAuthentication.*$/    PasswordAuthentication no/g" /etc/ssh/ssh_config':
+  path => '/usr/bin',
+}
+
+exec {'sudo sed -i "s/^.*id_rsa.*$/    IdentityFile ~\/.ssh\/school/g" /etc/ssh/ssh_config':
+  path => '/usr/bin',
+}
