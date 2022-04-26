e2096f6c135e934af7a5d07777523db894264aac
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-26 08:44:16 +0000

    add task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# Add a custom HTTP header with Puppet
+
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => present,
+  name    => 'nginx',
+  require => Exec['apt-get-update'],
+}
+
+file_line { 'header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
