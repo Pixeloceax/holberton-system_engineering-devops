6149e0afb9c1792e00d15f08f49d17c1674cf998
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-05-16 12:29:34 +0000

    add task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+"""
+from urllib import response
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    if len(argv) > 1:
+        response = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}".format(
+                argv[1]
+                )
+            )
+        User = (response.json())
+        response = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+                argv[1]
+            )
+        )
+        todos = (response.json())
+
+        number_done = 0
+        total = 0
+        task_done = []
+
+        for task in todos:
+            total += 1
+            if task.get("completed") is True:
+                number_done += 1
+                task_done.append(task.get("title"))
+
+        print(
+            "Employee {} is done with tasks({}/{}):".format(
+                User.get("name"), number_done, total)
+            )
+        for task in task_done:
+            print("\t {}".format(task))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+"""
+from urllib import response
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+
+    if len(argv) > 1:
+        response = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}".format(
+                argv[1]
+                )
+            )
+        User = (response.json().get("username"))
+        response = requests.get("https://jsonplaceholder.typicode.com/todos")
+        todos = (response.json())
+
+    file = argv[1] + ".csv"
+
+    with open(file, "w") as f:
+        writer = csv.writer(f,
+                            delimiter=",",
+                            quotechar='"',
+                            quoting=csv.QUOTE_ALL,
+                            lineterminator='\n')
+        for task in todos:
+            if task.get('userId') == int(argv[1]):
+                writer.writerow([argv[1], User, str(task.get("completed")),
+                                 task.get("title")])
