e2858086033bfa7a320d5bdba37d02a4da5f6149
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-05-31 09:49:58 +0000

    add task

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    1-top_ten.py
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+        top_ten
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'UA'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in range(10):
+            print(data.get
+                  ('data').get('children')
+                  [post].get('data').get('title'))
+    else:
+        return(None)
